227779c30eba845c9d87c2892cb083d95767a02e
HongShunYi <166114831+honsy107@users.noreply.github.com>
2025-07-26 19:08:45 +0800

    Update and rename MyOLED.h to MY_OLED.h

--- MY_OLED.h
+++ MY_OLED.h
@@ -1,8 +1,8 @@
 #include "main.h"
 #include "i2c.h"
 
-#ifndef __MYOLED_H__
-#define __MYOLED_H__
+#ifndef __MY_OLED_H__
+#define __MY_OLED_H__
 
 void MY_OLED_Command(uint8_t Command);
 void MY_OLED_CommandM(uint8_t Command[], int Num);
@@ -10,14 +10,14 @@ void MY_OLED_Data(uint8_t Data);
 void MY_OLED_DataM(uint8_t Data[], int Num);
 void MY_OLED_SetCursor(uint8_t x, uint8_t y);
 void MY_OLED_Full(uint8_t data);
-void MY_OLED_ShowP(uint8_t data[], int _Long, int _Hight, int _X, int _Y);
 void MY_OLED_Clear(void);
 void MY_OLED_Init(void);
-void MY_OLED_ShowD1(int X, int Y);
+void MY_OLED_ShowD1(int i1, int i2, int temp1, int X, int Y);
+//void MY_OLED_ShowDP(int i1, int i2, int temp1, int X, int Y);
 void MY_OLED_ShowSE(char StrE[], int X, int Y);
+void MY_OLED_ShowFS(float num, int size, int sn, int x, int y, int o);
 
-extern const uint8_t OLED_F8x16[][16];
-extern const uint8_t OLED_Chinese1[][32];
-extern const uint8_t OLED_Genshin[];
+
+extern const uint8_t OLED_ShowFout[][16];
 
 #endif
